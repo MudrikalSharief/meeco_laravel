379c8b25f4449899e98d5fe857a8e11857195500
coffee-Maker19 <125376601+coffee-Maker19@users.noreply.github.com>
2024-12-05 11:40:52 +0800

    composer open ai

--- resources/views/posts/reviewer.blade.php
+++ resources/views/posts/reviewer.blade.php
@@ -24,35 +24,8 @@
 
         <!-- Scrollable Content Box -->
         <div class="Reviewer border border-blue-500 rounded-lg bg-blue-50 p-6 overflow-y-scroll";>
-            <p class="font-semibold">Parts of Speech:</p>
-            <p>
-                Nouns: Words that name people, places, things, or ideas.<br>
-                Pronouns: Words that take the place of nouns.<br>
-                Verbs: Action words or words that express a state of being.<br>
-                Adjectives: Words that describe or modify nouns.<br>
-                Adverbs: Words that describe verbs, adjectives, or other adverbs.<br>
-                Prepositions: Words that show the relationship between a noun and another word.<br>
-                Conjunctions: Words that connect words, phrases, or clauses.<br>
-                Interjections: Words that express strong emotions.
-            </p>
-            <p class="font-semibold mt-4">Sentence Structures:</p>
-            <p>
-                Simple Sentence: Contains one independent clause.<br>
-                Compound Sentence: Contains two or more independent clauses.<br>
-                Complex Sentence: Contains one independent clause and at least one dependent clause.<br>
-                Compound-Complex Sentence: Contains two or more independent clauses and at least one dependent clause.
-            </p>
-            <p class="font-semibold mt-4">Tenses:</p>
-            <p>
-                Present Tense: Describes actions happening now.<br>
-                Past Tense: Describes actions that happened before.<br>
-                Future Tense: Describes actions that will happen.<br>
-                Present Perfect Tense: Describes actions that occurred at an unspecified time or started in the past and continue to the present.<br>
-                Past Perfect Tense: Describes actions completed before another action.<br>
-                Future Perfect Tense: Describes actions that will be completed by a certain future time.
-            </p>
-            <p class="font-semibold mt-4">Subject-Verb Agreement:</p>
-            <p>The subject and verb must agree in number. Singular subjects take singular verbs, while plural subjects take plural verbs.</p>
+            {{-- // Reviewer content here======================= --}}
+            <h1>Rviewer goes here</h1>
         </div>
 
         <div class="Rawtext hidden border border-blue-500 rounded-lg bg-blue-50 p-6 overflow-y-scroll";>
