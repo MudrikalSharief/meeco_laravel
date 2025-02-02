e06b67ff598caae85d902dcff7b0a505b81a6c6c
AlmcmhirTech <almcmhirservices@gmail.com>
2025-02-02 17:11:49 +0800

    updated routes
    yes

---
+++ resources/js/website/landing-page.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', () =>{
+    const learnMore1 = document.getElementById('learn-more-1');
+
+    learnMore1.addEventListener('click', ()=>{
+        window.location.href = ""
+
+    });
+
+});
