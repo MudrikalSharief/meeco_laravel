b588360245e7662a957888bc1c06de16e5aed448
RiceTrash <anthonyhuescaa@gmail.com>
2025-02-13 13:17:23 +0800

    Reapply "added datatable bootstrap"
    This reverts commit 921103881641363a29b2603490d66c230d7ff151.

--- resources/views/admin/admin_manage.blade.php
+++ resources/views/admin/admin_manage.blade.php
@@ -1,28 +1,37 @@
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+<link rel="stylesheet" href="//cdn.datatables.net/2.2.2/css/dataTables.dataTables.min.css">
 <x-admin_layout>
     <main>
-        <table class="min-w-full bg-white">
-            <thead>
-                <tr>
-                    <th class="py-2">Id</th>
-                    <th class="py-2">First Name</th>
-                    <th class="py-2">Last Name</th>
-                    <th class="py-2">Email</th>
-                    <th class="py-2">Date Created</th>
-                    <th class="py-2">Last Login</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach($admins as $admin)
+        <div class=" container mt-5">
+            <table id="myTable", class="min-w-full bg-white">
+                <thead>
                     <tr>
-                        <td class="border px-4 py-2">{{ $admin->admin_id }}</td>
-                        <td class="border px-4 py-2">{{ $admin->firstname }}</td>
-                        <td class="border px-4 py-2">{{ $admin->lastname }}</td>
-                        <td class="border px-4 py-2">{{ $admin->email }}</td>
-                        <td class="border px-4 py-2">{{ $admin->created_at }}</td>
-                        <td class="border px-4 py-2">{{ $admin->last_login }}</td>
+                        <th>Id</th>
+                        <th>First Name</th>
+                        <th>Last Name</th>
+                        <th>Email</th>
+                        <th>Date Created</th>
+                        <th>Last Login</th>
                     </tr>
-                @endforeach
-            </tbody>
-        </table>
+                </thead>
+                <tbody>
+                    @foreach($admins as $admin)
+                        <tr>
+                            <td>{{ $admin->admin_id }}</td>
+                            <td>{{ $admin->firstname }}</td>
+                            <td>{{ $admin->lastname }}</td>
+                            <td>{{ $admin->email }}</td>
+                            <td>{{ $admin->created_at }}</td>
+                            <td>{{ $admin->last_login }}</td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
     </main>
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+    <script src="//cdn.datatables.net/2.2.2/js/dataTables.min.js"></script>
+    <script>
+        let table = new DataTable('#myTable');
+    </script>
 </x-admin_layout>
