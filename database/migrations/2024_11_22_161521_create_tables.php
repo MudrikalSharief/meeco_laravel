43d926847f1418a7dc23759f81fd6586ef30fdbc
coffee-Maker19 <125376601+coffee-Maker19@users.noreply.github.com>
2025-02-16 22:32:16 +0800

    fix seeders for multiple choice

--- database/migrations/2024_11_22_161521_create_tables.php
+++ database/migrations/2024_11_22_161521_create_tables.php
@@ -121,7 +121,7 @@ return new class extends Migration
             $table->foreign('question_id')->references('question_id')->on('questions')->onDelete('cascade');
             $table->longText('question_text');
             $table->text('answer');
-            $table->text('answer');
+            $table->text('user_answer');
             $table->text('A');
             $table->text('B');
             $table->text('C');
--- resources/views/posts/takequiz.blade.php
+++ resources/views/posts/takequiz.blade.php
@@ -4,11 +4,11 @@
     <div class="z-50 bg-white px-6 py-3 fixed w-full shadow-lg">
         <p id="title" class=" text-blue-500"></p>
     </div>
-    <div class="max-w-2xl h-full mx-auto bg-white  rounded-lg">
+    <div class="max-w-2xl h-full mx-auto bg-white rounded-lg">
         
         <div class="w-full max-w-2xl">
 
-            <div class="quiz-container px-24 pt-24">
+            <div class="quiz-container lg:px-16 md:px-16 sm:px-10  px-5 pt-24">
                 <form id="quizForm">
                     <!-- Questions will be dynamically inserted here -->
                 </form>
