765dd0d73dfbf93d222c71b70424b92b8ac55498
RiceTrash <anthonyhuescaa@gmail.com>
2025-02-18 00:31:07 +0800

    test

--- resources/views/admin/admin_manage.blade.php
+++ resources/views/admin/admin_manage.blade.php
@@ -142,6 +142,4 @@
     </main>
     <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
     <script src="//cdn.datatables.net/2.2.2/js/dataTables.min.js"></script>
-    <script>
-    </script>
 </x-admin_layout>
