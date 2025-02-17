acbd90ca3860a240e29e547bcf4fff61d5fea848
RiceTrash <anthonyhuescaa@gmail.com>
2025-02-18 00:29:49 +0800

    transferred css

--- resources/views/admin/admin_manage.blade.php
+++ resources/views/admin/admin_manage.blade.php
@@ -1,6 +1,11 @@
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-<link rel="stylesheet" href="//cdn.datatables.net/2.2.2/css/dataTables.dataTables.min.css">
+
 <x-admin_layout>
+    <style>
+        .dt-input {
+            margin-right: 10px;
+        }
+    </style>
     <main>
         <div class="container mt-5">
             <table id="myTable" class="min-w-full bg-white">
