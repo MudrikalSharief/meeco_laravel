eb9ae364c84321586b0895a8f63f9bb7f1690d62
RiceTrash <anthonyhuescaa@gmail.com>
2025-02-19 23:56:55 +0800

    structure for contact us

--- resources/views/website/footer/contact.blade.php
+++ resources/views/website/footer/contact.blade.php
@@ -1,3 +1,35 @@
 <x-web_footer>
-        
+        <nav class="breadcrumb">
+            <a href="#">Home</a>
+            <span>></span>
+            <a href="#">Help and Support</a>
+        </nav>
+    
+        <h1>Contact Us</h1>
+        <p class="description">Thank you for choosing Meeco. Please feel free to contact us with the channel below, we are happy to help.</p>
+    
+        <div class="services">
+            <div class="service">
+                <svg class="icon" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M21 12C21 16.9706 16.9706 21 12 21C7.02944 21 3 16.9706 3 12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12Z" stroke="#87CEFA" stroke-width="2"/>
+                    <path d="M3 9L10.5 14.5L21 7" stroke="#87CEFA" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                </svg>
+                <div class="service-content">
+                    <h3>Inquiry</h3>
+                    <p>Got questions? This is your pathway to unravel the unknown.</p>
+                </div>
+            </div>
+    
+            <div class="service">
+                <svg class="icon" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M21 12C21 16.9706 16.9706 21 12 21C7.02944 21 3 16.9706 3 12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12Z" stroke="#87CEFA" stroke-width="2"/>
+                    <path d="M12 7V12L15 15" stroke="#87CEFA" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                </svg>
+                <div class="service-content">
+                    <h3>Inquiry History</h3>
+                    <p>See how your questions have shaped your quest for knowledge.</p>
+                </div>
+            </div>
+        </div>
+    
 </x-web_footer>
