b1583a50f13ef0af2628e1861779e6ea64605e72
RiceTrash <anthonyhuescaa@gmail.com>
2025-02-20 01:33:40 +0800

    converted to span

--- resources/views/website/footer/contact.blade.php
+++ resources/views/website/footer/contact.blade.php
@@ -3,7 +3,7 @@
         <div class="breadcrumb mb-10 pl-25 font-bold text-xl">
             <a href="{{ route('landing')}}" class="text-blue-700 no-underline">Home</a>
             <span class="text-blue-700 mx-1">></span>
-            <a href="#" class="text-blue-700 no-underline">Help and Support</a>
+            <span class="text-blue-700">Help and Support</span>
         </div>
     
         <h2 class="Contact_Us w-full text-center text-blue-700 text-4xl font-bold mb-5">Contact Us</h2>
