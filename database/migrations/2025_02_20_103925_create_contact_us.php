354510b04e3fa96ee29b4f10ee2a9ddc7be22f7a
RiceTrash <anthonyhuescaa@gmail.com>
2025-02-20 18:48:56 +0800

    Created database

---
+++ database/migrations/2025_02_20_103925_create_contact_us.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('contact_us', function (Blueprint $table) {
+            $table->id('ticket_id');
+            $table->string('ticket_reference')->unique();
+            $table->string('email');
+            $table->enum('category', ['Login Issue', 'Conversion Problem', 'Reveiwer Problem', 'Quiz Problem', 'Others']);
+            $table->text('question');
+            $table->json('upload')->nullable(); // To handle multiple pictures
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('contact_us');
+    }
+};
