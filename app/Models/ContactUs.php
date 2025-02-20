8c83fd3c4450877bab9f460a5ecad03951a8e2df
RiceTrash <anthonyhuescaa@gmail.com>
2025-02-20 22:41:39 +0800

    model for inquiry

---
+++ app/Models/ContactUs.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ContactUs extends Model
+{
+    use HasFactory;
+
+    protected $table = 'contact_us';
+
+    protected $fillable = [
+        'ticket_reference',
+        'email',
+        'category',
+        'question',
+        'upload',
+        'date_created',
+        'last_post'
+    ];
+
+    protected $casts = [
+        'upload' => 'array',
+        'date_created' => 'datetime',
+        'last_post' => 'datetime'
+    ];
+}
