8d14ef55caa3b249067e5632bb681d4487c8da65
RiceTrash <anthonyhuescaa@gmail.com>
2025-02-24 17:54:04 +0800

    border changed

--- resources/views/admin/admin_supportReply.blade.php
+++ resources/views/admin/admin_supportReply.blade.php
@@ -18,7 +18,7 @@
                 <div class="text-xs text-gray-500 mb-2">{{ $inquiry->created_at }}</div>
                 @if($inquiry->upload)
                     @foreach(json_decode($inquiry->upload) as $upload)
-                        <a href="{{ asset('storage/' . $upload) }}" class="bg-transparent border border-gray-300 text-gray-700 py-1 px-2 rounded-md hover:bg-gray-100 inline-block mb-1">Check Image</a>
+                        <a href="{{ asset('storage/' . $upload) }}" class="bg-transparent border border-gray-400 text-gray-700 py-1 px-2 rounded-md hover:bg-gray-100 inline-block mb-1">Check Image</a>
                     @endforeach
                 @endif
             </div>
@@ -38,7 +38,7 @@
                         <div class="text-xs text-gray-500 mb-2">{{ $reply->created_at }}</div>
                         @if($reply->reply_admin_upload)
                             @foreach(json_decode($reply->reply_admin_upload) as $upload)
-                                <a href="{{ asset('storage/' . $upload) }}" class="bg-transparent border border-gray-300 text-gray-700 py-1 px-2 rounded-md hover:bg-gray-100 inline-block mb-1">Check Image</a>
+                                <a href="{{ asset('storage/' . $upload) }}" class="bg-transparent border border-gray-400 text-gray-700 py-1 px-2 rounded-md hover:bg-gray-100 inline-block mb-1">Check Image</a>
                             @endforeach
                         @endif
                     </div>
@@ -52,7 +52,7 @@
                         <div class="text-xs text-gray-500 mb-2">{{ $reply->created_at }}</div>
                         @if($reply->reply_user_upload)
                             @foreach(json_decode($reply->reply_user_upload) as $upload)
-                                <a href="{{ asset('storage/' . $upload) }}" class="bg-transparent border border-gray-300 text-gray-700 py-1 px-2 rounded-md hover:bg-gray-100 inline-block mb-1">Check Image</a>
+                                <a href="{{ asset('storage/' . $upload) }}" class="bg-transparent border border-gray-400 text-gray-700 py-1 px-2 rounded-md hover:bg-gray-100 inline-block mb-1">Check Image</a>
                             @endforeach
                         @endif
                     </div>
