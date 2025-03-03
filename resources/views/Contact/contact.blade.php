a2e58783c6edc46ab0671f761690410549feb6e8
RiceTrash <anthonyhuescaa@gmail.com>
2025-03-04 01:46:11 +0800

    media changes

--- resources/views/Contact/contact.blade.php
+++ resources/views/Contact/contact.blade.php
@@ -9,21 +9,21 @@
         <h2 class="Contact_Us w-full text-center text-blue-700 text-4xl font-bold mb-5">Contact Us</h2>
         <p class="description text-center text-gray-800 mb-15">Thank you for choosing Meeco. Please feel free to contact us with the channel below, we are happy to help.</p>
     
-        <div class="services flex justify-center gap-24 mt-10">
-            <a href="{{ route('inquiry') }}">
-                <div class="service flex items-start gap-10 max-w-lg hover:bg-gray-200 rounded-lg p-9">
-                    <img class="icon w-10 h-10 object-contain mx-auto mt-3" src="{{ asset('logo_icons/pictures/note.png') }}" alt="Note">
-                    <div class="service-content">
+        <div class="services flex flex-col sm:flex-row justify-center items-center gap-6 sm:gap-12 md:gap-24 mt-10 px-2">
+            <a href="{{ route('inquiry') }}" class=" sm:w-auto mx-auto sm:mx-0">
+                <div class="service flex flex-col sm:flex-row items-center sm:items-start gap-4 sm:gap-10 max-w-lg hover:bg-gray-200 rounded-lg p-4 sm:p-9">
+                    <img class="icon w-10 h-10 object-contain mt-0 sm:mt-3" src="{{ asset('logo_icons/pictures/note.png') }}" alt="Note">
+                    <div class="service-content text-center sm:text-left">
                         <h3 class="text-blue-700 mb-2 text-lg font-bold">Inquiry</h3>
                         <p class="text-gray-800 m-0 leading-6">Got questions? This is your pathway to unravel the unknown.</p>
                     </div>
                 </div>
             </a>
             
-            <a href="{{ route('inquiry-history') }}">
-                <div class="service flex items-start gap-10 max-w-lg hover:bg-gray-200 rounded-lg p-9">
-                    <img class="icon w-10 h-10 object-contain mx-auto mt-3" src="{{ asset('logo_icons/pictures/history.png') }}" alt="Note">
-                    <div class="service-content">
+            <a href="{{ route('inquiry-history') }}" class=" sm:w-auto mx-auto sm:mx-0">
+                <div class="service flex flex-col sm:flex-row items-center sm:items-start gap-4 sm:gap-10 max-w-lg hover:bg-gray-200 rounded-lg p-4 sm:p-9">
+                    <img class="icon w-10 h-10 object-contain mt-0 sm:mt-3" src="{{ asset('logo_icons/pictures/history.png') }}" alt="Note">
+                    <div class="service-content text-center sm:text-left">
                         <h3 class="text-blue-700 mb-2 text-lg font-bold">Inquiry History</h3>
                         <p class="text-gray-800 m-0 leading-6">See how your questions have shaped your quest for knowledge.</p>
                     </div>
