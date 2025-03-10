0a2ff14e6bbadd0bebe407c11ba9fecf24960932
coffee-Maker19 <125376601+coffee-Maker19@users.noreply.github.com>
2025-03-11 00:41:45 +0800

    Update promos table, limit to not null

--- database/migrations/2025_02_20_153948_drop_columns_from_promos_table.php
+++ database/migrations/2025_02_20_153948_drop_columns_from_promos_table.php
@@ -16,12 +16,12 @@ return new class extends Migration {
             $table->timestamp('start_date')->nullable();
             $table->timestamp('end_date')->nullable();
             $table->enum('status', ['active', 'inactive'])->default('active');
-            $table->integer('image_limit')->nullable();
-            $table->integer('reviewer_limit')->nullable();
-            $table->integer('quiz_limit')->nullable();
-            $table->integer('quiz_questions_limit')->nullable();
-            $table->integer('can_mix_quiz')->nullable();
-            $table->integer('mix_quiz_limit')->nullable();
+            $table->integer('image_limit');
+            $table->integer('reviewer_limit');
+            $table->integer('quiz_limit');
+            $table->integer('quiz_questions_limit');
+            $table->integer('can_mix_quiz');
+            $table->integer('mix_quiz_limit');
             $table->timestamp('created_at')->nullable()->useCurrent();
             $table->timestamp('updated_at')->nullable()->useCurrentOnUpdate();
         });
