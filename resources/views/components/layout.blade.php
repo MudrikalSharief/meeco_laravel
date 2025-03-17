15cd58296c72bbfc67c3115bda3aa23e22051879
coffee-Maker19 <125376601+coffee-Maker19@users.noreply.github.com>
2025-03-18 06:52:08 +0800

    fix layout ui

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -97,25 +97,25 @@
         <ul id="menu" class="flex justify-between items-center px-4 mx-auto max-w-lg w-full">
             <a href="{{ route('capture') }}" class="bottom_nav block menu-item rounded-lg w-full {{ Request::routeIs('capture') ? 'bg-blue-100' : '' }}{{ Request::routeIs('extracted') ? 'bg-blue-100' : '' }}">
                 <li class="flex flex-col justify-start items-center gap-3 p-2">
-                    <div class="w-10  rounded-full flex justify-center items-center"><img class="filter-blue w-5" src="{{ asset('logo_icons/camera-viewfinder.svg') }}" alt="Icon"></div>
+                    <div class="w-10 flex justify-center items-center"><img class="filter-blue w-5" src="{{ asset('logo_icons/camera-viewfinder.svg') }}" alt="Icon"></div>
                     <p class="nav_text blue_text font-normal text-xs hidden sm:block">Convert New</p>
                 </li>
             </a>
             <a href="{{ route('subject') }}" class="bottom_nav block menu-item rounded-lg w-full {{ Request::routeIs('subjects') ? 'bg-blue-100' : '' }}{{ Request::routeIs('subject') ? 'bg-blue-100' : '' }}">
                 <li class="flex justify-start flex-col items-center gap-3 p-2">
-                    <div class="w-10= rounded-full flex justify-center items-center"><img class="filter-blue w-5" src="{{ asset('logo_icons/books.svg') }}" alt="Icon"></div>
+                    <div class="w-10 flex justify-center items-center"><img class="filter-blue w-5" src="{{ asset('logo_icons/books.svg') }}" alt="Icon"></div>
                     <p class="nav_text blue_text font-normal text-xs hidden sm:block">Subjects</p>
                 </li>
             </a>
-            <a href="{{ route('deleted') }}" class="bottom_nav block menu-item rounded-lg w-full {{ Request::routeIs('deleted') ? 'bg-blue-100' : '' }}">
+            {{-- <a href="{{ route('deleted') }}" class="bottom_nav block menu-item rounded-lg w-full {{ Request::routeIs('deleted') ? 'bg-blue-100' : '' }}">
                 <li class="flex justify-start flex-col items-center gap-3 p-2">
                     <div class="w-10 rounded-full flex justify-center items-center"><img class="filter-blue w-5" src="{{ asset('logo_icons/recycle-bin.svg') }}" alt="Icon"></div>
                     <p class="nav_text blue_text font-normal text-xs hidden sm:block">Deleted</p>
                 </li>
-            </a>
+            </a> --}}
             <a href="{{ route('upgrade') }}" class="bottom_nav block menu-item rounded-lg w-full {{ Request::routeIs('upgrade') ? 'bg-blue-100' : '' }}">
                 <li class="flex justify-start items-center flex-col gap-3 p-2">
-                    <div class="w-10 rounded-full flex justify-center items-center"><img class="filter-blue w-5" src="{{ asset('logo_icons/up.svg') }}" alt="Icon"></div>
+                    <div class="w-10 flex justify-center items-center"><img class="filter-blue w-5" src="{{ asset('logo_icons/up.svg') }}" alt="Icon"></div>
                     <p class="nav_text blue_text font-normal text-xs hidden sm:block">Upgrade</p>
                 </li>
             </a>
