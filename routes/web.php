91fb68d71a3b9c1a561601c0a73f398c2ba4705a
AlmcmhirTech <almcmhirservices@gmail.com>
2025-03-18 20:44:49 +0800

    dasdas
    dasdas

--- routes/web.php
+++ routes/web.php
@@ -267,7 +267,12 @@ Route::middleware(['auth:admin'])->group(function () {
 
     //Statistic Route
     Route::view('admin/statistics', 'admin.admin_statistics')->name('admin.statistics');
-    Route::get('admin/get-statistics', [StatisticsController::class, 'get_statistics'])->name('admin.get-statistics');
+
+    Route::post('admin/get-weekly-statistics', [StatisticsController::class, 'get_weekly_statistics'])->name('admin.get-statistics');
+    Route::post('admin/get-yearly-statistics', [StatisticsController::class, 'get_yearly_statistics'])->name('admin.get-statistics');
+    Route::post('admin/get-weekly-statistics', [StatisticsController::class, 'get_weekly_statistics'])->name('admin.get-statistics');
+    Route::post('admin/filter-yearly-statistics', [StatisticsController::class, 'filter_yearly_statistics'])->name('admin.get-statistics');
+    Route::post('admin/filter-weekly-statistics', [StatisticsController::class, 'filter_weekly_statistics'])->name('admin.get-statistics');
     //New Statistic Route
     Route::view('admin/newstatistics', 'admin.admin_newstatistics')->name('admin.newstatistics');
     Route::get('admin/subscription-stats', [SubscriptionController::class, 'getSubscriptionStats'])->name('admin.subscription-stats');
