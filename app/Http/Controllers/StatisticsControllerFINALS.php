d1883922cdeb8d0e1f77a4765a5abfb72b05f7f5
AlmcmhirTech <almcmhirservices@gmail.com>
2025-03-28 11:26:42 +0800

    Update StatisticsControllerFINALS.php

--- app/Http/Controllers/StatisticsControllerFINALS.php
+++ app/Http/Controllers/StatisticsControllerFINALS.php
@@ -33,12 +33,6 @@ class StatisticsController extends Controller
         $averageRevenue = ($totalSubs > 0) ? ($totalRevenue / $totalSubs) : 0;
 
         $daily_rev_arr = [];
-        
-        $recent_5_years = [];
-        for ($i = 0; $i < 5; $i++) {
-            $recent_5_years[] = Carbon::now()->subYears($i)->startOfYear()->year;
-        }
-        $recent_5_years_str = array_map('strval', $recent_5_years);
 
         $week_labels = [];
         for ($i = 0; $i < 7; $i++) {
@@ -82,43 +76,6 @@ class StatisticsController extends Controller
         ->groupBy('month')
         ->orderBy('month')
         ->get();
-
-        $monthly_rev =  Subscription::selectRaw('SUM(price) as total_amount')
-        ->leftJoin('promos', 'subscriptions.promo_id', '=', 'promos.promo_id')
-        ->where('subscriptions.start_date', '>=', $recent_january)
-        ->groupBy(DB::raw('DATE(subscriptions.start_date)'))
-        ->get();
-
-        $yearly_rev = Subscription::selectRaw('SUM(price) as total_amount')
-        ->leftJoin('promos', 'subscriptions.promo_id', '=', 'promos.promo_id')
-        ->where('subscriptions.start_date', '>=', $recent_5_years)
-        ->groupBy(DB::raw('DATE(subscriptions.start_date)'))
-        ->get();
-
-        $daily_ol = User::selectRaw('COUNT(user_id) as total_users')
-        ->where('last_login', '>=', $recent_monday)
-        ->groupBy(DB::raw('DATE(last_login)'))
-        ->get();
-
-        $monthly_ol = User::selectRaw('COUNT(user_id) as total_users')
-        ->where('last_login', '>=', $recent_january)
-        ->groupBy(DB::raw('DATE(last_login)'))
-        ->get();
-
-        $yearly_ol = User::selectRaw('COUNT(user_id) as total_users')
-        ->where('last_login', '>=', $recent_5_years)
-        ->groupBy(DB::raw('DATE(last_login)'))
-        ->get();
-
-        return response()->json([
-            'recent_5_years_str' => $recent_5_years_str,
-            'daily_rev' => $daily_rev,
-            'monthly_rev' => $monthly_rev,
-            'yearly_rev' => $yearly_rev,
-            'daily_ol' => $daily_ol,
-            'monthly_ol' => $monthly_ol,
-            'yearly_ol' => $yearly_ol
-        ]);
         
         $fromDate = Carbon::parse(trim($dateVals[0], '"')); 
         $toDate = Carbon::parse(trim($dateVals[1], '"'));
